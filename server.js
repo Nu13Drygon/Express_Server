6e7d25501348f9e22f227704ccd83ad64a5d7abc
Wrath <nu13drygon@gmail.com>
2022-09-21 09:01:41 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('success')
+})
+
+app.post('/login', (req, res) => {
+    if(req.body.password === 'cook' && req.body.email === 'cook@gmail.com') {
+        res.json('success')
+    } else {
+        res.json('failure')
+    }
+})
+
+app.listen(3001, () => {
+    console.log('app running on port:3001')
+})
